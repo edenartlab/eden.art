d5b039a3bb2b82975d687c44ccef9d05d080eab6
laubsauger <laubsauger@gmail.com>
2024-06-03 14:52:05 +0200

    center widget

--- src/components/waitlist/Widget.tsx
+++ src/components/waitlist/Widget.tsx
@@ -5,8 +5,8 @@ import styles from './Widget.module.css'
 
 const WaitListWidget = () => {
   return (
-    <div>
-      <div style={styles} id="getWaitlistContainer" data-waitlist_id="17436" data-widget_type="WIDGET_1"/>
+    <div className='flex justify-center items-center'>
+      <div style={styles} id="getWaitlistContainer" data-waitlist_id="17436" data-widget_type="WIDGET_1" className='w-full flex justify-center'/>
       <Script src="https://prod-waitlist-widget.s3.us-east-2.amazonaws.com/getwaitlist.min.js"/>
     </div>
   )
