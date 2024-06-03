4c31fa9af8fb98e3c4907b60b9d81c82912c577e
laubsauger <laubsauger@gmail.com>
2024-06-03 15:07:26 +0200

    defer load

--- src/components/waitlist/Widget.tsx
+++ src/components/waitlist/Widget.tsx
@@ -6,8 +6,19 @@ import styles from './Widget.module.css'
 const WaitListWidget = () => {
   return (
     <div className='flex justify-center items-center'>
-      <div style={styles} id="getWaitlistContainer" data-waitlist_id="17436" data-widget_type="WIDGET_1" className='w-full flex justify-center'/>
-      <Script src="https://prod-waitlist-widget.s3.us-east-2.amazonaws.com/getwaitlist.min.js"/>
+      <div id="getWaitlistContainer"
+           style={styles}
+           data-waitlist_id="17436"
+           data-widget_type="WIDGET_1"
+           className='w-full flex justify-center'
+      />
+      <Script
+        src="https://prod-waitlist-widget.s3.us-east-2.amazonaws.com/getwaitlist.min.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded: https://prod-waitlist-widget.s3.us-east-2.amazonaws.com/getwaitlist.min.js`)
+        }
+      />
     </div>
   )
 }
